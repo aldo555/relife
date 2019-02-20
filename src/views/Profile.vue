35179e6825dcd4e82a1e04269bad8751684fe0a5
Bogdan Mosteanu <aftermath@192-168-0-140.rdsnet.ro>
2019-02-20 22:54:53 +0200

    Added state manangement

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-2xl py-5">Hello, {{ me.name }}!</h1>
+    <h1 class="text-2xl py-5" v-if="me">Hello, {{ me.name }} !</h1>
     <button @click="logout" class="bg-white hover:bg-grey-lightest text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow">
       Logout
     </button>
@@ -13,26 +13,34 @@ import gql from 'graphql-tag'
 
 export default {
   name: 'Profile',
-  data() {
+
+  data () {
     return {
       me: null
     }
   },
   methods: {
-    logout() {
+    logout () {
       onLogout(this.$apollo.provider.defaultClient)
-      this.$router.push({name: 'Login'})
+      this.$router.push({ name: 'Login' })
     }
   },
   apollo: {
     me: gql`query {
       me {
-        id 
+        id
         name
         email
       }
     }
+    `,
+    hello: gql`
+      query {
+        hello @client {
+          msg
+        }
+      }
     `
-  }
+  },
 }
 </script>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -39,14 +39,14 @@ import { onLogin } from '../../vue-apollo.js'
 
 export default {
   name: 'Login',
-  data() {
+  data () {
     return {
       username: '',
       password: ''
     }
   },
   methods: {
-    login() {
+    login () {
       this.$apollo.mutate({
         // Query
         mutation: gql`mutation($data: LoginInput!) {
@@ -65,7 +65,7 @@ export default {
         // Result
         console.log(data)
         onLogin(this.$apollo.provider.defaultClient, data.data.login.access_token)
-        this.$router.push({name: 'Profile'})
+        this.$router.push({ name: 'Profile' })
       }).catch((error) => {
         // Error
         console.error(error)

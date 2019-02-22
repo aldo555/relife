f2bd1eb8702160452b3a26d4afa4da06e377941a
Aldo <aldodumitrescu@gmail.com>
2019-02-22 22:59:01 +0200

    accept and reject call and proper disconnect

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -9,22 +9,22 @@
         Couldn't establish connection to server
       </h5>
 
-      <video v-if="callSrc" width="1280" height="720" autoplay class="center-block" :srcObject.prop='callSrc'></video>
+      <video v-if="conn && conn.open && callSrc" width="480" height="360" autoplay class="center-block" :srcObject.prop='callSrc'></video>
       
-      <div v-if="!stream && incomingCall">
-        <h5 v-if="!conn.open" class="block text-grey-darker text-sm font-bold mt-5 mb-2" for="email">
-          <strong class="text-green-dark">{{caller}}</strong> wants to connect with you
+      <div v-if="!callSrc && incomingCall">
+        <h5 v-if="conn && !conn.open" class="block text-grey-darker text-sm font-bold mt-5 mb-2" for="email">
+          <strong class="text-green-dark">{{connectionId}}</strong> wants to connect with you
         </h5>
         <button :disabled='!peer' @click="acceptConnection" :class="{ 'hover:bg-green opacity-50 cursor-not-allowed' : !peer  }" class="bg-green w-full hover:bg-green-light my-3 text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow">
           Connect
         </button>
-        <button @click="declineConnection" class="bg-white hover:bg-red-light text-white font-semibold py-2 px-4 border bg-red rounded shadow">
-          Disconnect
+        <button @click="declineConnection" class="bg-white hover:bg-red-light text-white font-semibold py-2 px-4 border bg-red rounded shadow w-full">
+          Reject
         </button>
       </div>
 
-      <form v-if="!stream && !incomingCall" @submit.prevent="connect">
-        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
+      <form v-if="!callSrc && !incomingCall && !conn || (conn && !conn.open)" @submit.prevent="connect">
+        <label class="block text-grey-darker text-sm font-bold mb-2">
           Your friend's connection id
         </label>
         <input v-model="connectionId" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" required type="text" placeholder="ID">
@@ -33,19 +33,17 @@
         </button>
       </form>
 
-      <div v-else>
-        <h5 v-if="!conn.open" class="block text-grey-darker text-sm font-bold mt-5 mb-2" for="email">
-          Connecting...
-        </h5>
-        <h5 v-else class="block text-green-dark text-sm font-bold mt-5 mb-2" for="email">
-          Connected to {{connectionId}}
-        </h5>
-        <button @click="disconnect" class="bg-white hover:bg-red-light text-white font-semibold py-2 px-4 border bg-red rounded shadow">
-          Disconnect
-        </button>
-      </div>
+      <h5 v-if="conn && conn.open &&!callSrc && stream" class="block text-grey-darker text-sm font-bold mt-5 mb-2">
+        Connecting...
+      </h5>
+      <h5 v-if="conn && conn.open && callSrc" class="block text-green-dark text-sm font-bold mt-5 mb-2">
+        Connected to {{connectionId}}
+      </h5>
+      <button v-if="conn && conn.open && stream" @click="disconnect" class="bg-white hover:bg-red-light text-white font-semibold py-2 px-4 border bg-red rounded shadow">
+        Disconnect
+      </button>
 
-      <button @click="logout" class="bg-white hover:bg-grey-lightest mt-5 text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow">
+      <button @click="logout" class="bg-white hover:bg-grey-lightest mt-10 text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow">
         Logout
       </button>
     </div>
@@ -65,12 +63,11 @@ export default {
       me: null,
       peer: null,
       incomingCall: false,
-      caller: '',
       conn: null,
       call: null,
       stream:null,
       callSrc: null,
-      connectionId: ''
+      connectionId: '',
     }
   },
   methods: {
@@ -78,28 +75,16 @@ export default {
       onLogout(this.$apollo.provider.defaultClient)
       this.$router.push({name: 'Login'})
     },
+
     connect() {
       let that = this;
-      this.conn = this.peer.connect(this.connectionId)
-      //peer.on('connection', function(conn) { cut and insert the code block below here });
+      this.conn = this.peer.connect(this.connectionId, { metadata: { name: this.me.name } })
 
-      this.conn.on('open', function() {
-        console.log('Connected to ' + that.connectionId)
-
-        // Receive messages
-        that.conn.on('data', function(data) {
-          console.log(data)
-        });
-
-        // Send messages
-        that.conn.send('Greetings from ' + that.me.name);
-      });
-
-      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      navigator.mediaDevices.getUserMedia({ audio: false, video: true })
       .then(function(stream) {
         that.stream = stream
         that.call = that.peer.call(that.connectionId, that.stream)  
-        that.call.on('that.stream', function(stream) {
+        that.call.on('stream', function(stream) {
           that.callSrc = stream
         });
       })
@@ -107,12 +92,39 @@ export default {
         console.log(err)
       });
     },
+
     disconnect() {
-      if (this.stream) {
-        this.stream.getTracks().forEach(function (track) { track.stop(); });
+      if(this.stream) {
+        this.stream.getTracks().forEach(function (track) { track.stop(); })
       }
-      this.stream = ''
+      this.conn.close()
+      this.conn = null
+      this.call = null
+      this.stream = null
+      this.callSrc = null
       this.connectionId = ''
+    },
+
+    acceptConnection() {
+      let that = this;
+      navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+      .then(function (stream) {
+        that.stream = stream
+        that.call.answer(that.stream)
+        that.call.on('stream', function(stream) {
+          that.callSrc = stream
+        });
+        that.incomingCall = false
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    },
+
+    declineConnection() {
+      this.call.close();
+      this.conn.close();
+      this.incomingCall = false
     }
   },
   mounted() {
@@ -123,27 +135,63 @@ export default {
       config: {
         iceServers: [
           { url: 'stun:stun.l.google.com:19302' },
-          { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' }
+          { url: 'turn:aftermath-studio.go.ro:3478', credential: 'Acasa55@', username: 'aftermath' }
         ]
       }
     }
     this.peer = new Peer(Math.random().toString(36).substr(2, 5), params)
     this.peer.on('open', function(id) {
       console.log('My peer ID is: ' + id)
-    });
-    this.peer.on('call', function (call) {
-      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-      .then(function (stream) {
-        that.stream = stream
-        call.answer(that.stream)
-        call.on('stream', function(stream) {
-          that.callSrc = stream
-        });
-      })
-      .catch(function (err) {
-        console.log('caca')
-      })
     })
+
+    let that = this;
+    
+    this.peer.on('connection', function(conn) {
+      that.conn = conn
+
+      that.conn.on('close', function() {
+        console.log('call ended')
+        if(that.stream) {
+          that.stream.getTracks().forEach(function (track) { track.stop(); })
+        }
+        that.conn = null
+        that.call = null
+        that.stream = null
+        that.callSrc = null
+        that.connectionId = ''
+      })
+    });
+
+    this.peer.on('call', function (call) {
+      that.call = call
+      that.connectionId = call.peer
+      that.incomingCall = true;
+    })
+  },
+  updated() {
+    //checking if call was ended by answerer
+    if (this.conn && !this.conn.open && this.callSrc) {
+      if(this.stream) {
+        this.stream.getTracks().forEach(function (track) { track.stop(); })
+      }
+      this.conn = null
+      this.call = null
+      this.stream = null
+      this.callSrc = null
+      this.connectionId = ''
+    }
+
+    //checking if call was rejected
+    if (this.conn && !this.conn.open && this.call && !this.call.open) {
+      if(this.stream) {
+        this.stream.getTracks().forEach(function (track) { track.stop(); })
+      }
+      this.conn = null
+      this.call = null
+      this.stream = null
+      this.callSrc = null
+      this.connectionId = ''
+    }
   },
   apollo: {
     me: gql`query {

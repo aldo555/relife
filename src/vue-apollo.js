a8f9862c931ae6e1fba2eeee6de1a091d5fae1ea
Dumitrescu Aldo <aldodumitrescu@gmail.com>
2019-02-23 00:12:25 +0200

    temporary endpoint for testing purposes

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://relife.test:60/graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://acf9a260.ngrok.io/graphql'
 
 // Config
 const defaultOptions = {
